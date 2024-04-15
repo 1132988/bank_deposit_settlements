11619c46b2fcf8cb992f0183eb5960895576ace7
Dmitrii <ddeal087@yandex.ru>
2024-04-15 16:04:31 +0300

    modified:   deposit.py

--- deposit.py
+++ deposit.py
@@ -1,13 +1,21 @@
 #Стандартный вклад в банке, под свои параметры
-s=int(input("Сумма вклада: "))
-m=int(input("Количество месяцев: "))
-p=int(input("Количество процентов годовых: "))
+import matplotlib.pyplot as plt
+s = int(input("Сумма вклада: "))
+m = int(input("Количество месяцев: "))
+p = int(input("Количество процентов годовых: "))
+months = []
+sums = []
+
 for i in range(1, m+1):
-    s=(s * p/100/12) + s #расчёт суммы с набежавшими процентами каждый месяц
+    s = (s * p/100/12) + s
+    months.append(i)
+    sums.append(s)
     print('Сумма на ', i, 'месяц = ', s)
     s=s//1
-print("Общая сумма", s)
+print('Общая сумма: ', s)
 
-
-
-
+plt.plot(months, sums)
+plt.xlabel('Месяцы')
+plt.ylabel('Сумма на вкладе')
+plt.title('Динамика изменения суммы вклада')
+plt.show()
